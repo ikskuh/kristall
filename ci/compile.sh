ae85907fd307651dc52c3cf7d439c04882c24562
Felix (xq) Queißner <git@mq32.de>
2020-06-29 19:16:32 +0200

    Makes linux AppImage build reproducible with Docker.

--- ci/compile.sh
+++ ci/compile.sh
@@ -8,6 +8,25 @@ git clone https://github.com/MasterQ32/kristall /kristall
 
 cd /kristall
 
-make
+make -j$(nproc)
+
+mkdir -p AppDir/usr/bin
+mkdir -p AppDir/usr/share/doc/libc6/
+
+make install DESTDIR=AppDir PREFIX=/usr/
+
+export VERSION=nightly
+
+# Create copyright notice that linuxdeployqt wants
+cp /usr/share/doc/libc6/copyright AppDir/usr/share/doc/libc6/copyright
+
+echo "Running linuxdeployqt"
+linuxdeployqt AppDir/usr/share/applications/Kristall.desktop -unsupported-allow-new-glibc -appimage
+
+echo "Running linuxdeploy"
+linuxdeploy --appdir AppDir --library=/usr/lib/x86_64-linux-gnu/libssl.so.1.1 --output=appimage
 
 cp kristall /artifacts
+
+cp Kristall-nightly-$(uname -m).AppImage /artifacts/
+
